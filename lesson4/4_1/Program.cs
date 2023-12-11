47a09487d7c8b34a7a563a21b2cccc6bb5ee54e4
andreytsvl15 <andreytsvl15@gmail.com>
2023-12-11 10:57:12 +0300

    4_1

---
+++ lesson4/4_1/Program.cs
@@ -0,0 +1,58 @@
+﻿// 1. Задайте одномерный массив, заполненный случайными
+// числами. Определите количество простых чисел в этом
+// массиве.
+
+void Print(int[] arr)
+{
+int size = arr.Length;
+
+for (int i = 0; i < size; i++)
+{
+Console.Write($"{arr[i]} ");
+}
+Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+int[] arr = new int[size];
+
+for (int i = 0; i < size; i++)
+arr[i] = new Random().Next(from, to + 1);
+return arr;
+}
+
+bool IsPrime(int number)
+{
+if (number < 2)
+return false;
+
+for (int i = 2; i <= Math.Sqrt(number); i++)
+{
+if (number % i == 0)
+return false;
+}
+return true;
+}
+
+int SimpleNum(int[] arr)
+{
+int count = 0;
+
+foreach (int number in arr)
+{
+if (IsPrime(number))
+count++;
+}
+return count;
+}
+
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+int result = SimpleNum(mass);
+Console.WriteLine(result);
