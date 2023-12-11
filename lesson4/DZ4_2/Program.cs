4e869dabf06cc28881d81b0249c3f02bf0cedf7e
andreytsvl15 <andreytsvl15@gmail.com>
2023-12-11 11:58:29 +0300

    DZ4_2

---
+++ lesson4/DZ4_2/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int size = 10;
+
+        int[] numbers = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            numbers[i] = random.Next(99, 1000);
+        }
+
+        Console.WriteLine("массив:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+
+        int count = 0;
+        foreach (int number in numbers)
+        {
+            if (number % 2 == 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("Количество четных чисел: " + count);
+
+      
+    }
+}
