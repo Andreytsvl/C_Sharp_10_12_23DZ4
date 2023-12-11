71f57db795d985f71897a49512bcc01a60ec27e2
andreytsvl15 <andreytsvl15@gmail.com>
2023-12-11 11:30:51 +0300

    4_3

---
+++ lesson4/4_3/Program.cs
@@ -0,0 +1,39 @@
+﻿void Print(int[] arr)
+{
+int size = arr.Length;
+
+for (int i = 0; i < size; i++)
+{
+Console.Write($"{arr[i]} ");
+}
+Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+int[] arr = new int[size];
+
+for (int i = 0; i < size; i++)
+arr[i] = new Random().Next(from, to + 1);
+return arr;
+}
+
+long CountNum(int[] arr)
+{
+    string word = "";
+
+foreach (int number in arr)
+{
+    word += number;
+}
+return long.Parse(word);
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+long result = CountNum(mass);
+Console.WriteLine(result);
